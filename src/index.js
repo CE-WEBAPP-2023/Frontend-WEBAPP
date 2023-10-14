4e559f6e28cb9335e40d9b1576c9d571d39f7153
saveffer1 <saveffer01@gmail.com>
2023-10-14 20:24:08 +0700

    Restructure of the app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import Navbar from './components/Nav/NavBar';
+import './index.css';
+import Home from './pages/Home/Home';
+import About from './pages/AboutUs/About';
+import PageNotFound from './pages/404/PageNotFound';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Navbar content = {
+        <div style={{width:"100%",height:'100%'}}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </div>
+    }/>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return(
+        <div className='homediv'>
+            <h1>Busy?</h1>
+            <button className="button-primary roundbtn">Let's Go.</button>
+        </div>
+    );
+}
+
+export default Home;
