d822747708a3f37608cec983293b3c3286576a6e
saveffer1 <saveffer01@gmail.com>
2023-10-22 12:10:18 +0700

    move axios to node fetch

--- src/pages/OrderCanteen/OrderInCanteen.js
+++ src/pages/OrderCanteen/OrderInCanteen.js
@@ -1,74 +1,66 @@
-// import logo from './logo.svg';
 import React, { useEffect, useState } from 'react';
 import { APIURL } from '../../config';
-import axios from 'axios';
 import OrderCard from '../../components/OrderInCanteen_Pickup/OrderCard';
 import '../../components/OrderInCanteen_Pickup/OrderInCanteen.css'
-import { useParams } from 'react-router-dom'; 
+import { useParams } from 'react-router-dom';
+import serverError from '../500/ServerError';
 
 function OrderInCanteen() {
   const [data, setData] = useState([]);
-  const [loading,setLoading] = useState(false);
-  const canteenName = ["A","J","C","พระเทพ"]
+  const [loading,setLoading] = useState(true);
   const { canId } = useParams();
   const Cid = parseInt(canId);
-  // const canId = 1;
+  const [canteenNames, setCanteenNames] = useState([]);
   useEffect(() => {
-
-    
-
-    const fetchData = async () => {
+    async function fetchData() {
       try {
-        const response = await axios.get(`${APIURL}/Order/all`);
-        const responseData = response.data;
-        console.log(responseData);
-        setData(responseData.filter((can) => (can.canteen.canteenId === Cid)))
-        console.log(data);
-        setLoading(false);
-      } catch (error) {
-        console.error(error);
-        setLoading(true);
-      }
-    };
+        const canteenResponse = await fetch(`${APIURL}/Canteen`);
+        const canteenData = await canteenResponse.json();
+        const names = canteenData.map((can) => can.canteenName);
 
-    fetchData(); 
-  }, []);
+        if (names && names.length > 0 && names[Cid-1] !== undefined) {
+          const orderResponse = await fetch(`${APIURL}/Order/all`);
+          const orderData = await orderResponse.json();
+
+          const filteredData = orderData.filter((order) => order.canteen.canteenId === Cid);
+          setData(filteredData);
+          setCanteenNames(names);
+          setLoading(false);
+        } else {
+          window.location.href = '/404';
+        }
+      } catch (error) {
+        return (
+          serverError()
+        )
+      }
+    }
+
+    fetchData();
+  }, [Cid]);
   
-      return (
+  return (
     <div className="OrderInCanteen">
 
-     {loading ? ( <div className='loadingpage'> <div className='loader'></div> </div> ) : ( 
-     <div className='bigBoy'>
-      <div className='OrderInCanteen-header'> โรงอาหาร {canteenName[Cid-1]} </div>
-        
+      {loading ? ( <div className='loadingpage'> <div className='loader'></div> </div> ) : ( 
+      <div className='bigBoy'>
+        <div className='OrderInCanteen-header'> โรงอาหาร {canteenNames[Cid-1]} </div>
         <div className='OrderALL'>
-         
           {data.map((order, index) => (
-            
-            <OrderCard key={index} fname={order.user.name}lname={order.user.lastName} phonetel={order.user.phoneNumber} userlocation={order.userLocation} food={order.food} orderid={order._id} />
-                
-            
+            <OrderCard 
+              key={index} 
+              fname={order.user.name}
+              lname={order.user.lastName} 
+              phonetel={order.user.phoneNumber} 
+              userlocation={order.userLocation} 
+              food={order.food} 
+              orderid={order._id} 
+            />
           ))} 
-         </div>
-
-
-
-
-
-     </div> )} 
-    
-      
+        </div>
+      </div> )} 
     </div>
   );
 }
-
-
-
-
-
-
-
-
-
 
 export default OrderInCanteen;
